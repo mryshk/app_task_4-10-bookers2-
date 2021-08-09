ed30bda6f21d57eee64f925552e590fde7224ff4
mryshk <ec2-user@ip-172-31-87-102.ec2.internal>
2021-08-09 13:24:21 +0000

    [update]8/9_2

---
+++ bookers2/app/controllers/books_controller.rb
@@ -0,0 +1,36 @@
+class BooksController < ApplicationController
+
+  def show
+    @book = Book.new
+    @book = Book.find(params[:id])
+  end
+
+  def index
+
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:opinion)
+  end
+
+
+end
+
