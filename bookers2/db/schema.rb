2aa93104dac668f5f4ab12c12b19897631404703
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-08-16 09:45:01 +0000

    [modified]8_16

--- bookers2/db/schema.rb
+++ bookers2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_10_020147) do
+ActiveRecord::Schema.define(version: 2021_08_16_091147) do
 
   create_table "books", force: :cascade do |t|
     t.integer "user_id"
