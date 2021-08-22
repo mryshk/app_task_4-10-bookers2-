642c2a9edcc9ec3e4ef5483e8bca4cf5d9d830a3
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-08-22 13:48:31 +0000

    [update]AddFavoriteAjaxButThereAreProblems

--- bookers2/app/controllers/favorites_controller.rb
+++ bookers2/app/controllers/favorites_controller.rb
@@ -4,17 +4,12 @@ class FavoritesController < ApplicationController
     @favorite = Favorite.new(book_id: @book.id)
     @favorite.user_id = current_user.id
     @favorite.save
-    redirect_back(fallback_location: root_path)
-
   end
 
   def destroy
      @book = Book.find(params[:book_id])
      @favorite = Favorite.find_by(book_id: @book.id,user_id: current_user.id)
      @favorite.destroy
-    redirect_back(fallback_location: root_path)
   end
-
-
 
 end
