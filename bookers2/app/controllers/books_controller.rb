b72cf7d06484efa103a5202e7eaf9784c0892ffb
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-08-30 09:29:06 +0000

    [update]AddGroupViews

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
   def show
     @book_s = Book.new
     @book = Book.find(params[:id])
-    @user = User.find(@book.user_id)
+    @user = User.find(curre)
     @book_comment = BookComment.new
   end
 
--- bookers2/app/controllers/groups_controller.rb
+++ bookers2/app/controllers/groups_controller.rb
@@ -1,34 +1,43 @@
 class GroupsController < ApplicationController
   def new
-    @group = Group
-    
+    @group = Group.new
+    @group.users << current_user
   end
-  
+
   def create
-    
+    @group = Group.new(group_params)
+    @group.owner_id = current_user
+    @group.save
+    redirect_to groups_path
   end
-  
+
   def show
-    
-  end 
-  
+    @group = Group.find(params[:id])
+    @book_s = Book.new
+    @user = User.find(current_user.id)
+  end
+
   def index
-    
+    @groups = Group.all
+    @book_s = Book.new
+    @user = User.find(current_user.id)
   end
-  
+
   def edit
-    
+
   end
-  
+
   def update
-    
+
   end
-  
+
   def destroy
-    
+
   end
-  
+
   private
-  
-  
+  def group_params
+    params.require(:group).permit(:name,:introduction,:image)
+  end
+
 end
