cb9b27fb2f4246b424e3f352b950a44a8b2ec90c
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-08-30 08:13:03 +0000

    [update]AddGroup

---
+++ bookers2/app/controllers/groups_controller.rb
@@ -0,0 +1,34 @@
+class GroupsController < ApplicationController
+  def new
+    @group = Group
+    
+  end
+  
+  def create
+    
+  end
+  
+  def show
+    
+  end 
+  
+  def index
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  private
+  
+  
+end
