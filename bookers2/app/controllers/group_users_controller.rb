ab75f5b4f760cbd9f549c67c28f3858cd40ab85c
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-08-31 02:06:05 +0000

    [update]ModifiedGitignore

--- bookers2/app/controllers/group_users_controller.rb
+++ bookers2/app/controllers/group_users_controller.rb
@@ -1,12 +1,10 @@
 class GroupUsersController < ApplicationController
   def create
     @group = Group.find(params[:group_id])
-    unless current_user.id == @group.owner_id
       @group_user = GroupUser.new(group_id: @group.id)
       @group_user.user_id = current_user.id
       @group_user.save
       redirect_to group_path(@group)
-    end
   end
 
   def destroy
